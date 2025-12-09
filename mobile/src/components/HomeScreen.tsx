27ead9df6cc52c45d2958ad685ced4dd7e9033af
Dev Ekkx <devekkx@gmail.com>
2025-12-09 09:36:25 +0000

    feature: create homescreen component

--- mobile/src/components/HomeScreen.tsx
+++ mobile/src/components/HomeScreen.tsx
@@ -1,11 +1,22 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import LocationIcon from "./LocationIcon";
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
       <LocationIcon />
+
+      <Text style={styles.text}>Use Location</Text>
+
+      <Text style={styles.desc}>
+        When you click on the use location button, your location will be used to
+        get your position.
+      </Text>
+
+      <TouchableOpacity style={styles.btn}>
+        <Text style={styles.btnText}>Get Location</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -17,5 +28,32 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    flexDirection: "column",
+    gap: 24,
+    paddingInline: 24,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "black",
+  },
+  desc: {
+    fontSize: 16,
+    color: "black",
+    textAlign: "center",
+  },
+  btn: {
+    backgroundColor: "#001797ff",
+    paddingInline: 24,
+    paddingBlock: 16,
+    borderRadius: 8,
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  btnText: {
+    color: "#FFFFFF",
+    fontSize: 18,
   },
 });
